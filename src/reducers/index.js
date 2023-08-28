83fbb6bc22dcb1a278981d72f14b989bb1d70074
Megan Huckabay <huckabay90@gmail.com>
2023-08-28 12:21:29 -0700

    updated

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,11 @@
 import { combineReducers } from 'redux';
 
 import movieReducer from './movieReducer';
+import FavoriteReducer from '../components/FavoriteMovieList';
 
-export default movieReducer;
+const rootReducer = combineReducers({
+    movieReducer,
+    FavoriteReducer
+});
+
+export default rootReducer;
